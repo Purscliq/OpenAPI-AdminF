fe10889aed1157e4feef20f1df83a0ae1d42405c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-05-20 22:20:48 +0100

    chore: sider

--- components/dashboard-components/dashboard/Dashboard.tsx
+++ components/dashboard-components/dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
-"use client";
 import React from "react";
+import Image from "next/image";
+import Emoji from "@/assets/png/Happy-Emoji-PNG 1.png";
 
 const Dashboard = () => {
   return (
     <section className="max-w-[1640px] flex flex-col gap-6 bg-[#FAFAFA] px-6 py-4 md:h-screen overflow-y-scroll">
       <span className="">
-        <h2 className="text-[24px] font-bold mb-1">Dashboard</h2>
+        <span className="flex gap-2 items-center">
+          <h2 className="text-[24px] font-bold mb-1">Dashboard</h2>
+          <Image src={Emoji} alt="Emoji" className="w-[24px]" />
+        </span>
         <p className="text-[#262626] text-sm">
           Open the panel and watch your progress and growth in knowledge.
         </p>
--- components/layout/DashData.tsx
+++ components/layout/DashData.tsx
@@ -1,6 +1,12 @@
 import { MenuProps } from "antd";
 import Link from "next/link";
 import DashboardIcon from "@/assets/svg/DashboardIcon";
+import ComplianceIcon from "@/assets/svg/ComplianceIcon";
+import BusinessIcon from "@/assets/svg/BusinessIcon";
+import TransactionsIcon from "@/assets/svg/TransactionsIcon";
+import RolesIcon from "@/assets/svg/RolesIcon";
+import SettingsIcon from "@/assets/svg/SettingsIcon";
+import LogOutIcon from "@/assets/svg/LogOutIcon";
 
 export const sidebarData1: MenuProps["items"] = [
   {
@@ -10,44 +16,44 @@ export const sidebarData1: MenuProps["items"] = [
   },
   {
     label: <Link href="/compliance">Compliance</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <ComplianceIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "compliance",
   },
   {
     label: <Link href="/account">Account</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <ComplianceIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "account",
   },
   {
     label: <Link href="/business">Business</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <BusinessIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "business",
   },
   {
     label: <Link href="/transactions">Transactions</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <TransactionsIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "transactions",
   },
   {
     label: <Link href="/developers">Developers</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <RolesIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "developers",
   },
   {
     label: <Link href="/roles-and-permissions">Roles and permissions</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <RolesIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "roles-and-permissions",
   },
   {
     label: <Link href="/settings">Settings</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <SettingsIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "settings",
   },
 ];
 export const sidebarData2: MenuProps["items"] = [
   {
     label: <Link href="/">Log Out</Link>,
-    icon: <DashboardIcon className=" font-bold text-[#7C8493] w-4 h-4" />,
+    icon: <LogOutIcon className=" font-bold text-[#7C8493] w-5 h-5" />,
     key: "logout",
   },
 ];
